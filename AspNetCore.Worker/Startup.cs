1afab2741c81140683337896427b25b6ad5ca231
Nahid Hasan <nahid675@diu.edu.bd>
2018-11-12 09:23:28 +0600

    First Commit

---
+++ AspNetCore.Worker/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+
+namespace AspNetCore.Worker
+{
+    class Startup
+    {
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            /// Dependency Injection Implementation Here Or Inject Servie Collection
+            /// services.AddTransit<>();
+
+            // This is service provider for create instance of DI object
+            ServiceProvider provider = services.BuildServiceProvider();
+
+            // This is an example of how to create instance of DI object
+            //objectName obj = provider.GetService<objectName>();
+
+            // Now you will able to execute your desired process
+
+        }
+    }
+}
